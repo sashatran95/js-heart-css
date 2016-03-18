ec20de566be908b5b745752e645c9e0b727e62f3
Sashtastic <ht.sasha.tran@gmail.om>
2016-03-18 14:41:41 -0400

    Done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
 var $header = $('header');
-$header.addClass('masthead');
-
-
-
-
 var $h1 = $('h1');
-$h1.subrtactClass('big-heading');
-$h1.addClass('logo');
-
-
-
 var $main = $('main');
 var $main = $('main');
 var $h2 = $('h2');
-var $display = $('ul li:last-child');
+var $show = $('ul li:last-child');
 
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
 
 $main.addClass('wrapper');
 $('p:first-child').addClass('intro');
 $h2.addClass('slug-head');
 $('main ul').addClass('slug-list');
 $('img').addClass('slug-img');
-$('li:first-child').addClass('slug-list-first')
-$('li:last-child').addClass('slug-list-last')
+$('li:first-child').addClass('slug-list-first');
+$('li:last-child').addClass('slug-list-last');
 $('dl').addClass('classification');
-$('dt').subrtactClass('big-label');
-$display.display();
+$('dt').removeClass('big-label');
+$show.show();
