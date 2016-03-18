8902f587ef3904bd6a0ffd9562f74d615ac37415
Sashtastic <ht.sasha.tran@gmail.om>
2016-03-18 14:37:40 -0400

    Javascription

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var $header = $('header');
+$header.addClass('masthead');
+
+
+
+
+var $h1 = $('h1');
+$h1.subrtactClass('big-heading');
+$h1.addClass('logo');
+
+
+
+var $main = $('main');
+var $main = $('main');
+var $h2 = $('h2');
+var $display = $('ul li:last-child');
+
+
+$main.addClass('wrapper');
+$('p:first-child').addClass('intro');
+$h2.addClass('slug-head');
+$('main ul').addClass('slug-list');
+$('img').addClass('slug-img');
+$('li:first-child').addClass('slug-list-first')
+$('li:last-child').addClass('slug-list-last')
+$('dl').addClass('classification');
+$('dt').subrtactClass('big-label');
+$display.display();
